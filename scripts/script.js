3f922a3df279d86c59bc5477e9d662b573a7ee98
MickvdBoom <mickvdboom@gmail.com>
2024-11-27 15:41:19 +0100

    hamburger en overig toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,16 @@
-// JavaScript Document
-console.log("hi");
+var openButton = document.querySelector("header section button");
+var sluitButton = document.querySelector("nav button");
+var deNav = document.getElementById("hamburgerMenu")
+
+openButton.addEventListener("click", openMenu)
+sluitButton.addEventListener("click", sluitMenu)
+
+function openMenu() {  
+//    console.log("menu is geopend")
+    deNav.classList.add("toonMenu");
+}
+
+function sluitMenu() {  
+    // console.log("menu is gesloten")
+     deNav.classList.remove("toonMenu");
+ }
