3d6ce2da3917bbb4b503cebe21690f71d30ac53c
MickvdBoom <mickvdboom@gmail.com>
2024-11-30 14:13:22 +0100

    2e nav werkend

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,14 @@
 var openButton = document.querySelector("header div button");
 var sluitButton = document.querySelector("nav button");
 var deNav = document.getElementById("hamburgerMenu")
+var openNavTwee = document.querySelector(".knopTwee a")
+var sluitNavTwee = document.querySelector(".navTwee button")
+var navTwee = document.querySelector(".navTwee")
 
 openButton.addEventListener("click", openMenu)
 sluitButton.addEventListener("click", sluitMenu)
+openNavTwee.addEventListener("click", openTweede)
+sluitNavTwee.addEventListener("click", sluitTweede)
 
 function openMenu() {  
 //    console.log("menu is geopend")
@@ -13,4 +18,14 @@ function openMenu() {
 function sluitMenu() {  
     // console.log("menu is gesloten")
      deNav.classList.remove("toonMenu");
+}
+
+function openTweede() {
+    // console.log("hooiii")
+    navTwee.classList.add("toonMenuTwee")
+}
+
+function sluitTweede() {
+    // console.log("kaaaasss")
+    navTwee.classList.remove("toonMenuTwee");
 }
